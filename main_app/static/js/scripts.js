e41cbc631958cfd57f7a2606bf5d97d2903440a2
KingSri <srirang97@gmail.com>
2020-05-25 17:31:26 -0700

    added working links to homepage

--- main_app/static/js/scripts.js
+++ main_app/static/js/scripts.js
@@ -1,1 +1,9 @@
 console.log('Masked, I advance.');
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
